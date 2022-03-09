182bae5faad222b54a0a681052e949ca5234601c
lmjpawaon <lmjpawaon@addu.edu.ph>
2022-03-09 08:29:58 +0800

    update index

--- index.js
+++ index.js
@@ -30,10 +30,8 @@ app.use('/subjects',appRouter);
 app.use('/404',appRouter);
 
 app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get('env') === 'development' ? err : {};
-  // render the error page
     res.status(err.status || 500);
     res.render('404');
 });
